ae52585da3b43a6685b5f61d28c84367aad48c43
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-10-08 01:14:35 +0100

    🛠️ refactor ♻️(layout): Comment out Header component
    - Comment out the Header component in the main page layout
    - Introduce client-side state management for navbar visibility
    - Use React hooks to toggle the navbar menu
    - Update the rendering of the menu icon for better UX

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Navbar />
       <main className="background-gradient flex h-screen w-screen items-center justify-center">
         <div className="w-[90%] space-y-2 lg:w-[60rem] lg:space-y-10">
-          <Header />
+          {/* <Header /> */}
           <div className="flex h-[65vh]">
             <ChatSection />
           </div>
--- frontend/components/blocks/navbar.tsx
+++ frontend/components/blocks/navbar.tsx
@@ -1,4 +1,12 @@
+'use client'
+
+import * as React from "react";
+import { Menu } from "lucide-react";
+
+
+
 export default function Navbar() {
+    const [show, setShow] = React.useState(false);
     return (
         <nav className="border-gray-200 bg-white dark:bg-gray-900">
             <div className="mx-auto flex w-full flex-wrap items-center justify-between p-4 px-12">
@@ -15,28 +23,15 @@ export default function Navbar() {
                 <button
                     data-collapse-toggle="navbar-default"
                     type="button"
-                    className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+                    onClick={() => setShow(!show)}
+                    className={`${show ? '' : 'order-2'} inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:order-none md:hidden`}
                     aria-controls="navbar-default"
                     aria-expanded="false"
                 >
                     <span className="sr-only">Open main menu</span>
-                    <svg
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 17 14"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15"
-                        />
-                    </svg>
+                    <Menu />
                 </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                <div className={`${show ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
                     <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse">
                         {['Home', 'About', 'Services', 'Pricing', 'Contact'].map(
                             (item, index) => (
